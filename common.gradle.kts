55dd303183aff1b77e3e092645fe3ac3fe30dbc1
vqhuy <vqhuy@axonactive.com>
2025-10-06 14:17:39 +0700

    update config

---
+++ common.gradle.kts
@@ -0,0 +1,16 @@
+repositories {
+  mavenCentral()
+  maven("https://oss.sonatype.org/content/repositories/snapshots/")
+}
+
+rootProject.extensions.add("platform.version", "LATEST-EAP-SNAPSHOT")
+
+rootProject.extensions.add("gradle.version", "8.5")
+
+rootProject.extensions.add("junit.version", "4.13.2")
+
+rootProject.extensions.add("kotlin.jvmTarget", "21")
+rootProject.extensions.add("kotlin.freeCompilerArgs", listOf("-Xjvm-default=all"))
+
+rootProject.extensions.add("java.sourceCompatibility", "21")
+rootProject.extensions.add("java.targetCompatibility", "21")
--- src/main/kotlin/com/gravity/drools/DroolsLanguage.kt
+++
@@ -1,5 +0,0 @@
-package com.gravity.drools
-
-import com.intellij.lang.Language
-
-object DroolsLanguage : Language("Drools")
